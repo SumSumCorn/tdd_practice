73ae14a8e28bddb0fc12b613c4fc5774c65fe196
SumSumCorn <a01035587180@gmail.com>
2022-04-01 05:18:11 +0900

    init server setting

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 const express = require('express');
 
 const PORT = 8081;
@@ -5,12 +6,13 @@ const app = express();
 const productRoutes = require('./routes');
 const mongoose = require('mongoose');
 
-mongoose.connect("mongodb+srv://catch22:zcb135@test1.qeunp.mongodb.net/testdb?retryWrites=true&w=majority",
+mongoose.connect(process.env.MONGODB_URI,
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-  }).then(()=> console.log('mongodb connected...')
-  ).catch((err)=> console.log(err))
+  }, () => {
+    console.log('mongodb connected...')
+  });
 
 app.use(express.json());
 
